3781bd40b20df2c3a7a2503555a80bfa24fe1d09
pratikshah98 <39916172+pratikshah98@users.noreply.github.com>
2022-02-13 10:46:58 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+
+import { AppComponent } from "./app.component";
+import { SurveyComponent } from "./components/survey.component";
+
+@NgModule({
+    declarations: [AppComponent, SurveyComponent],
+    imports: [BrowserModule],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
